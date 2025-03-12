2b1a156a7841ced00400f792c048ef4a279d71fc
Andrew LeTourneau <centerorbit@gmail.com>
2025-03-12 12:19:08 -0500

    feat: new cap touch

--- drivekeys.ino
+++ drivekeys.ino
@@ -1,52 +1,177 @@
-#include <Arduino.h>
-#include <BleKeyboard.h>
+/*********************************************************************
+ This is an example for our nRF52 based Bluefruit LE modules
 
-#define BUTTON1 26 
-#define BUTTON2 25 
-#define BUTTON3 13 
+ Add this board manager:
+ https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
 
-BleKeyboard bleKeyboard(
-  "DriveKeys",
-  "ShipLift LLC",
-  100
-);
+ Pick one up today in the adafruit shop!
 
-int button1State = HIGH;
-int button2State = HIGH;
-int button3State = HIGH;
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
 
-void setup() {
-  bleKeyboard.begin();
-  pinMode(BUTTON1, INPUT_PULLUP);
-  pinMode(BUTTON2, INPUT_PULLUP);
-  pinMode(BUTTON3, INPUT_PULLUP);
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+// # Product name  : Capacitive touch pad 
+// # Version  : 0.4
+// # Power supply: 3.3v
+// # Connection:
+// #       SDA (MPR121) -> PIN A4 (Arduino)
+// #       SCL (MPR121) -> PIN A5 (Arduino)
+// #       IRQ (MPR121) -> PIN A2 (Arduino) 
+#include <bluefruit.h>
+#include <Wire.h>
+#include <mpr121.h>
+
+BLEDis bledis;
+BLEHidAdafruit blehid;
+
+int X ;           // X-coordinate
+int Y ;           // Y-coordinate
+
+#define LED_RED 13
+
+bool hasKeyPressed = false;
+
+void setup() 
+{
+  Wire.begin();
+  CapaTouch.begin();
+
+  Serial.begin(115200);
+  while ( !Serial ) delay(10);   // for nrf52840 with native usb
+
+  Serial.println("Bluefruit52 HID Keyboard Example");
+  Serial.println("--------------------------------\n");
+
+  Serial.println();
+  Serial.println("Go to your phone's Bluetooth settings to pair your device");
+  Serial.println("then open an application that accepts keyboard input");
+
+  Serial.println();
+  Serial.println("Enter the character(s) to send:");
+  Serial.println();  
+
+  Bluefruit.begin();
+  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather 52");
+  bledis.begin();
+
+  /* Start BLE HID
+   * Note: Apple requires BLE device must have min connection interval >= 20m
+   * ( The smaller the connection interval the faster we could send data).
+   * However for HID and MIDI device, Apple could accept min connection interval 
+   * up to 11.25 ms. Therefore BLEHidAdafruit::begin() will try to set the min and max
+   * connection interval to 11.25  ms and 15 ms respectively for best performance.
+   */
+  blehid.begin();
+
+  // Set callback for set LED from central
+  blehid.setKeyboardLedCallback(set_keyboard_led);
+
+  /* Set connection interval (min, max) to your perferred value.
+   * Note: It is already set by BLEHidAdafruit::begin() to 11.25ms - 15ms
+   * min = 9*1.25=11.25 ms, max = 12*1.25= 15 ms 
+   */
+  /* Bluefruit.Periph.setConnInterval(9, 12); */
+
+  // Set up and start advertising
+  startAdv();
 }
 
-void stateChecker(int button, int *state, uint8_t key){
-  int currentButton1State = digitalRead(button);
-  if (currentButton1State != *state)
-    {
-        if (currentButton1State == LOW)
-        {
-            bleKeyboard.press(key);
-        }
-        else
-        {
-            bleKeyboard.release(key);
-        }
+void startAdv(void)
+{  
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addAppearance(BLE_APPEARANCE_HID_KEYBOARD);
+  
+  // Include BLE HID service
+  Bluefruit.Advertising.addService(blehid);
+
+  // There is enough room for the dev name in the advertising packet
+  Bluefruit.Advertising.addName();
+  
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
+}
+
+void loop() 
+{
+  // Only send KeyRelease if previously pressed to avoid sending
+  // multiple keyRelease reports (that consume memory and bandwidth)
+  if ( hasKeyPressed )
+  {
+    hasKeyPressed = false;
+    blehid.keyRelease();
+    
+    // Delay a bit after a report
+    delay(5);
+  }
+    
+  if (Serial.available())
+  {
+    char ch = (char) Serial.read();
+
+    // echo
+    Serial.write(ch); 
+
+    blehid.keyPress(ch);
+    hasKeyPressed = true;
+    
+    // Delay a bit after a report
+    delay(5);
+  }
+
+  {
+    X=CapaTouch.getX();              // Get X position.
+    Y=CapaTouch.getY();              // Get Y position.
+    if(X>=1&&X<=9&&Y>=1&&Y<=13)
+    {                                 // Determine whether in the range.If not,do nothing.
+        Serial.print("X=");
+        Serial.print(X);
+        Serial.print("  Y=");
+        Serial.println(Y);
+        blehid.keySequence(("X=" + String(X) + " Y=" + String(Y)).c_str());
     }
-    *state = currentButton1State;
+    delay(200);
+  }
 }
 
-void loop() {
-  if(bleKeyboard.isConnected()) {
-    stateChecker(BUTTON1, &button1State, KEY_LEFT_CTRL); // SW
-    stateChecker(BUTTON2, &button2State, KEY_LEFT_ALT); // SE
-    stateChecker(BUTTON3, &button3State, KEY_LEFT_SHIFT); // NE
-
-    // Or, actual keys (for debugging etc)
-    // stateChecker(BUTTON1, &button1State, 'a'); // SW
-    // stateChecker(BUTTON2, &button2State, 'b'); // SE
-    // stateChecker(BUTTON3, &button3State, 'c'); // NE
+/**
+ * Callback invoked when received Set LED from central.
+ * Must be set previously with setKeyboardLedCallback()
+ *
+ * The LED bit map is as follows: (also defined by KEYBOARD_LED_* )
+ *    Kana (4) | Compose (3) | ScrollLock (2) | CapsLock (1) | Numlock (0)
+ */
+void set_keyboard_led(uint16_t conn_handle, uint8_t led_bitmap)
+{
+  (void) conn_handle;
+  
+  // light up Red Led if any bits is set
+  if ( led_bitmap )
+  {
+    ledOn( LED_RED );
+  }
+  else
+  {
+    ledOff( LED_RED );
   }
 }
