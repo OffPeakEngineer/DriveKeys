4d64312699ff0c9a464e23505e2cf0d8e6fa6727
Andrew LeTourneau <centerorbit@gmail.com>
2025-04-16 22:12:36 -0500

    feat: added a keybinding lookup for touch id

--- drivekeys.ino
+++ drivekeys.ino
@@ -1,7 +1,3 @@
-//
-//
-
-
 #include <Arduino.h>
 #include <BleKeyboard.h>
 #include <Adafruit_NeoPixel.h>
@@ -14,6 +10,7 @@
 
 // How many internal neopixels do we have? some boards have more than one!
 #define NUMPIXELS        1
+#define MAX_TOUCH_IDS    10
 
 int X ;           // X-coordinate
 int Y ;           // Y-coordinate
@@ -21,14 +18,51 @@ int Y ;           // Y-coordinate
 struct KeyBinding {
   uint8_t keycode; // HID keycode (e.g., KEY_RETURN, 'A', etc.)
   bool isPressed; 
-  uint16_t touchIDs[140];
+  uint16_t touchIDs[MAX_TOUCH_IDS];
 };
 
+
+
+
+// I've drawn a black sharpie dot on the touchpad that I want to regularly
+// target. Knowing that I'll likely trigger multiple touchpads as I attempt
+// to reach just this one. (think Operation Game)
+
+// So for this example, I've "trained" the touchpad to recognize an 'A'
+// for any of the touch ID signals that it recieves. Notice there are
+// roughtly 4 different touch IDs that will appear as I press this dot.
+// They directly correlate to the touchpad grids that I'm overlapping upon
+// with my fat fingers.
+
+
+
 KeyBinding keyBindings[] = {
-  { 'A', false, {256, 384, 400}},
-  { 'B', false, {}},
-  { 'C', false, {}},
+  // { keycode, isPressed, { touchIDs } } // Null terminated
+  { 'A', false, {1028, 1032, 1036, 1024, 0}},
+  { 'B', false, {0}},
+  { 'C', false, {0}},
 };
+
+
+
+// The "bug", or just issue I have to work through now is how to accurately
+// track the 'release' of a touchpad. That search is a little different.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+String pressKeys = "";
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 Adafruit_MPR121 cap = Adafruit_MPR121();
@@ -59,6 +93,9 @@ void drawKeyMatrix(unsigned long keyMatrix[10][14]);
 void yield(int delay);
 void printTouchLine(uint16_t touched);
 bool isKeyPressed(char key);
+char getKeyForTouchID(uint16_t id);
+KeyBinding* findKeyBinding(char key);
+
 
 void setup() {
   Wire.begin();
@@ -130,6 +167,7 @@ void loop() {
     // or, every frame drawn was processed durring the last frames display.
     drawKeyMatrix(keyMatrix);
     // Above:      "  +-------------------------------+
+    // End of drawing grid, on to stats and metrics.
     Serial.println("                  Missing: " + missing);
     Serial.println("  Event Log:");
     for (int i = 0; i < msgHistLen; i++) {
@@ -141,7 +179,8 @@ void loop() {
     }
     Serial.println("");
     Serial.println("               Touch ID: #" + String(currtouched)+"   ");
-    Serial.print("          "); printTouchLine(currtouched);
+    Serial.print("          ");   printTouchLine(currtouched);
+    Serial.print("Press Keys: "); Serial.println(pressKeys);
   }
 
   // Continually check the touchpad until a difference is found.
@@ -155,36 +194,62 @@ void loop() {
 
   int x = getX(currtouched);
   int y = getY(currtouched);
+  int X = getX(lasttouched);
+  int Y = getY(lasttouched);
   if (x > 0 && y > 0) {
     for (uint8_t i=0; i<12; i++) {
       // it if *is* touched and *wasnt* touched before, alert!
       if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
-        int from = keyMatrix[x][y];
-        if (from == 0) {
-          keyMatrix[x][y] = millis();
-
-          msgNm++;
-          eventLog[msgNm % msgHistLen] = String(msgNm) + ". (" + String(x) + "," + String(y) + ") --> pressed.";
-
-          if(bleKeyboard.isConnected()) {
-              // bleKeyboard.press(key);
+        char key = getKeyForTouchID(currtouched);
+        if (key != '\0') { // We found a key
+          // Lookup the keybinding
+          KeyBinding* keypress = findKeyBinding(key);
+          // Check if the key needs to be pressed
+          if (!keypress->isPressed) {
+            keypress->isPressed = true;
+            pressKeys += key;
+            if(bleKeyboard.isConnected()) {
+                bleKeyboard.press(key);
+            }
           }
+        }
+        
+        
+        if (keyMatrix[x][y] == 0) {
+          keyMatrix[x][y] = millis();
+          msgNm++;
+          eventLog[msgNm % msgHistLen] = String(msgNm) + ". (" + String(x) + "," + String(y) + ") =" + String(key) + " --> pressed.    ";
         }
       }
 
       // if it *was* touched and now *isnt*, alert!
+      // Also note the switch in context from "currtouched" to "lasttouched"
       if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-        if (keyMatrix[x][y] != 0) {
-          // keyMatrix[x][y] = 0;
+
+        char key = getKeyForTouchID(lasttouched);
+        if (key != '\0') { // We found a key
+          // Lookup the keybinding
+          KeyBinding* keypress = findKeyBinding(key);
+          // Check if the key is pressed, if so, release it
+          if (keypress->isPressed) {
+            keypress->isPressed = false;
+            pressKeys.remove(pressKeys.indexOf(key), 1);
+            if(bleKeyboard.isConnected()) {
+                bleKeyboard.release(key);
+            }
+          }
+        }
+
+        // Update the drawn keyMatrix
+        if (keyMatrix[X][Y] != 0) {
+          // keyMatrix[X][Y] = 0;
           // Normally this would be a reset,
           //    but I'm treating it as "any detected activity"
-          keyMatrix[x][y] = millis();
+          // aka: "no longer touching this area" does mean "this area was recently disturbed"
+          //      and so we are (re)marking it as a sign of human activity.
+          keyMatrix[X][Y] = millis();
           msgNm++;
-          eventLog[msgNm % msgHistLen] = String(msgNm) + ". (" + String(x) + "," + String(y) + ") --> released.";
-          if(bleKeyboard.isConnected()) {
-            if (getKeyForTouchIDisKeyPressed) 
-              // bleKeyboard.press(key);
-          }
+          eventLog[msgNm % msgHistLen] = String(msgNm) + ". (" + String(X) + "," + String(Y) + ") =" + String(key) + " --> released.    ";
         }
       }
     }
@@ -214,17 +279,6 @@ void yield(int ms) {
   delay(ms);
 }
 
-
-bool isKeyPressed(char key) {
-  for (size_t i = 0; i < sizeof(keyBindings) / sizeof(KeyBinding); i++) {
-    if (keyBindings[i].keycode == key) {
-      return keyBindings[i].isPressed;
-    }
-  }
-  return false; // Not found or not pressed
-}
-
-
 char getKeyForTouchID(uint16_t id) {
   for (size_t i = 0; i < sizeof(keyBindings) / sizeof(KeyBinding); i++) {
     const KeyBinding &binding = keyBindings[i];
@@ -232,7 +286,7 @@ char getKeyForTouchID(uint16_t id) {
     for (size_t j = 0; j < MAX_TOUCH_IDS; j++) {
       uint16_t tid = binding.touchIDs[j];
 
-       if (tid == 0xFFFF) break;
+       if (tid == 0) break;
 
       if (tid == id) {
         return binding.keycode;
@@ -240,6 +294,15 @@ char getKeyForTouchID(uint16_t id) {
     }
   }
   return '\0'; // No match
+}
+
+KeyBinding* findKeyBinding(char key) {
+  for (size_t i = 0; i < sizeof(keyBindings) / sizeof(KeyBinding); i++) {
+    if (keyBindings[i].keycode == key) {
+      return &keyBindings[i];
+    }
+  }
+  return nullptr; // Not found
 }
 
 void drawKeyMatrix(unsigned long keyMatrix[10][14]) {
